6828e48f5fe9274f52983b03d9de83e45861aea1
nadiaf85 <nadia.fiorio@alice.it>
2021-12-10 16:09:36 +0100

    quasi finito

---
+++ js/myscript.js
@@ -0,0 +1,17 @@
+let scatola = document.getElementById ('container'); //richiamo l'id
+
+for (let i=0 ; i <=100 ; i++){ //creo ciclo for
+
+    if ((i % 3==0) && (i % 5==0)){ //controllo se è multiplo di 3 e 5 e stampo fizzbuzz
+        scatola.innerHTML += '<div class= box fizzbuzz>' + i + '</div>';
+    }
+    else if ((i % 3==0)) //controllo se è multiplo di 3 e stampo fizz
+        scatola.innerHTML += '<div class= box fizz>' + i + '</div>';
+    } 
+    if ((i % 5==0)){ //controllo se è multiplo di 5 e stampo buzz
+        scatola.innerHTML += '<div class= box buzz>' + i + '</div>';
+    }
+    else{
+        document.getElementById ('container');
+        scatola.innerHTML += '<div class= box>' + i + '</div>';
+    }
