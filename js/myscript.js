4bc4f102cccb28681338057c7799a496dc12893c
nadiaf85 <nadia.fiorio@alice.it>
2021-12-10 17:04:16 +0100

    modifiche varie

--- js/myscript.js
+++ js/myscript.js
@@ -2,16 +2,18 @@ let scatola = document.getElementById ('container'); //richiamo l'id
 
 for (let i=0 ; i <=100 ; i++){ //creo ciclo for
 
+
     if ((i % 3==0) && (i % 5==0)){ //controllo se è multiplo di 3 e 5 e stampo fizzbuzz
-        scatola.innerHTML += '<div class= box fizzbuzz>' + i + '</div>';
+        scatola.innerHTML += '<div class= box fizzbuzz>' + i + 'fizzbuzz </div>';
     }
-    else if ((i % 3==0)) //controllo se è multiplo di 3 e stampo fizz
-        scatola.innerHTML += '<div class= box fizz>' + i + '</div>';
+    else if (i % 3==0) {//controllo se è multiplo di 3 e stampo fizz
+        scatola.innerHTML += '<div class= box fizz>' + i + 'fizz </div>';
     } 
-    if ((i % 5==0)){ //controllo se è multiplo di 5 e stampo buzz
-        scatola.innerHTML += '<div class= box buzz>' + i + '</div>';
+    else if (i % 5==0){ //controllo se è multiplo di 5 e stampo buzz
+        scatola.innerHTML += '<div class= box buzz>' + i + 'buzz </div>';
     }
     else{
         document.getElementById ('container');
         scatola.innerHTML += '<div class= box>' + i + '</div>';
     }
+}
